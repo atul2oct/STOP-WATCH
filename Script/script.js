005924cbe7b62bed9feb40c64040a6919bc3f271
atul2oct <www.myfoot@gmail.com>
2023-03-10 01:33:37 +0530

    Update script.js

--- Script/script.js
+++ Script/script.js
@@ -1,5 +1,22 @@
-window.onload = function () {
-    //getting data
+window.onload = () => {
+    //setting initial values
+    var startWatch = false;
+    var ms = 0;
+    var seconds = 0;
+    var minutes = 0;
+    var timeInterval = null;
+    var LapNumber = 0;
+    var previousMin = 0;
+    var previousSec = 0;
+    var previousMsec = 0;
+    var previousMinimum = "59 : 59 . 99";
+    var previousMinimumId = 1;
+    var previousMaximum = "00 : 00 . 00";
+    var previousMaximumId = 0;
+    var play_pauseAudio = new Audio("Script/Audio/Start.mp3");
+    var lapAudio = new Audio("Script/Audio/Stop.mp3");
+
+    //getting data from html
     var Minute = document.querySelector("#min");
     var Second = document.querySelector("#sec");
     var MilliSecond = document.querySelector("#msec");
@@ -7,32 +24,22 @@ window.onload = function () {
     var Stop = document.querySelector(".stop");
     var Reset = document.querySelector(".reset");
     var lapBtn = document.querySelector(".lap-btn");
-    var tableLap = document.querySelector("table");
-    var copyBtn = document.querySelector(".copy-btn");
-    //setting initial values
-    var startClock = false;
-    var ms = 0;
-    var seconds = 0;
-    var minutes = 0;
-    var timeInterval = null;
-    var i = 0;
-    var laps = 0;
-    var prevMin = 0;
-    var prevSec = 0;
-    var prevMsec = 0;
-    var prevMinimum = "59 : 59 . 99";
-    var prevMinimumId = 1;
-    var prevMaximum = "00 : 00 . 00";
-    var prevMaximumId = 0;
-    var startAudio = new Audio("Audio/Start.mp3");
-    var lapAudio = new Audio("Audio/Stop.mp3");
+    var LapTable = document.querySelector("table");
 
+    //button animation when pressed
+    function animateBtn(btn) {
+        var activeButton = document.querySelector("#" + btn);
+        activeButton.classList.add("pressed");
+        setTimeout(function () {
+            activeButton.classList.remove("pressed");
+        }, 100)
+    }
     //start button function
     Start.addEventListener("click", function () {
-        if (!startClock) {
-            startAudio.play();
+        if (!startWatch) {
+            play_pauseAudio.play();
             animateBtn(this.innerHTML);
-            startClock = true;
+            startWatch = true;
             //after every 10ms function run
             timeInterval = setInterval(function () {
                 //for millisecond
@@ -68,11 +75,11 @@ window.onload = function () {
 
     //stop button function
     Stop.addEventListener("click", function stop() {
-        if (startClock) {
-            startAudio.play();
+        if (startWatch) {
+            play_pauseAudio.play();
             animateBtn(this.innerHTML)
             clearInterval(timeInterval);
-            startClock = false;
+            startWatch = false;
             //after stopwatch stop it will show start and reset buttons only
             Start.style.display = "inline-block"
             Stop.style.display = "none"
@@ -84,13 +91,13 @@ window.onload = function () {
     //Restart button function
     Reset.addEventListener("click", function () {
         animateBtn(this.innerHTML);
-        startAudio.play();
+        play_pauseAudio.play();
         clearInterval(timeInterval);
-        if (startClock) {
+        if (startWatch) {
             Stop.style.display = "none"
             Start.style.display = "inline-block"
         }
-        startClock = false;
+        startWatch = false;
         //reset the time to 00:00.00
         ms = 0;
         seconds = 0;
@@ -98,22 +105,163 @@ window.onload = function () {
         Minute.innerHTML = "00"
         Second.innerHTML = "00"
         MilliSecond.innerHTML = "00"
-        tableLap.style.display = "none";
-        prevMin = 0;
-        prevSec = 0;
-        prevMsec = 0;
-        prevMaximum = "00 : 00 . 00"
-        prevMinimum = "59 : 59 . 99"
-        // laps = 0;
+        LapTable.style.display = "none";
+        previousMin = 0;
+        previousSec = 0;
+        previousMsec = 0;
+        previousMaximum = "00 : 00 . 00"
+        previousMinimum = "59 : 59 . 99"
         removeLaps();
     });
 
-    //button animation when pressed
-    function animateBtn(btn) {
-        var activeButton = document.querySelector("#" + btn);
-        activeButton.classList.add("pressed");
-        setTimeout(function () {
-            activeButton.classList.remove("pressed");
-        }, 100)
+    //lap button function
+    lapBtn.addEventListener("click", function () {
+        lapAudio.play();
+        animateBtn(this.innerHTML);
+        LapTable.style.display = "table";
+        lap();
+    });
+
+    //Lap function
+    var tableRow = document.querySelector("tr");
+    function lap() {
+        LapNumber+=1;
+        var row = document.createElement("tr")
+        row.classList.add("remove");
+        row.setAttribute("id", LapNumber);
+        var srNo = document.createElement("td");
+        //--note input for remarks for each lap times--//
+        var note = document.createElement("td");
+        var input = document.createElement("input");
+        input.type = "text";
+        input.placeholder = "Add Note"
+        input.classList.add("remarks");
+        note.appendChild(input);
+        //--Lap and overall times--//
+        var currentLap = document.createElement("td");
+        var overallTime = document.createElement("td");
+        var deleteLap = document.createElement("td");
+        //adding 0 to single digit serial number
+        srNo.innerHTML = LapNumber < 10 ? "0" + LapNumber : LapNumber;
+        //every individual lap time
+        currentLap.innerHTML = DifferenceBetweenLaps(minutes, seconds, ms);
+        overallTime.innerHTML = (minutes < 10 ? "0" + minutes : minutes) + " : " + (seconds < 10 ? "0" + seconds : seconds) + " . " + (ms < 10 ? "0" + ms : ms);
+        //delete icon for removing lap row from table
+        var deleteIcon = document.createElement("Button");
+        deleteIcon.classList.add("bin");
+        deleteIcon.innerHTML = ('<i class="fa-solid fa-trash-can"></i>')
+        //inserting into columns
+        deleteLap.appendChild(deleteIcon);
+        row.appendChild(srNo);
+        row.appendChild(note);
+        row.appendChild(currentLap);
+        row.appendChild(overallTime);
+        row.appendChild(deleteLap);
+        tableRow.after(row);
+        //for delete button
+        document.querySelector(".bin").addEventListener("click", function () {
+            var currentRow = this.parentNode.parentNode;
+            currentRow.classList.add('remove-active');
+            setTimeout(() => {
+                currentRow.remove();
+            }, 500);
+        })
+        //minimum and maximum time amongst all lap times
+        MinMaxOfLapTime(currentLap.innerHTML, LapNumber);
+    }
+    //Deleting lap row function
+    function removeLaps() {
+        var createdLaps = document.querySelectorAll(".remove");
+        for (var i = 0; i < LapNumber; i++) {
+            createdLaps[i].remove();
+        }
+        LapNumber = 0;
+    }
+
+    //function for calculating lap time
+    function DifferenceBetweenLaps(currentMinutes, currentSeconds, currentMs) {
+        var diffMin = 0;
+        var diffSec = 0;
+        var diffMsec = 0;
+
+        if (currentMinutes > previousMin) {
+            currentMinutes--;
+            currentSeconds += 60;
+        }
+
+        if (previousMsec > currentMs) {
+            currentSeconds--;
+            currentMs += 100;
+            diffMsec = currentMs - previousMsec;
+            diffSec = currentSeconds - previousSec;
+            diffMin = currentMinutes - previousMin;
+        }
+
+        else if (previousMsec <= currentMs) {
+            diffMsec = currentMs - previousMsec;
+            diffSec = currentSeconds - previousSec;
+            diffMin = currentMinutes - previousMin;
+        }
+
+        var diffTime = ((diffMin < 10 ? "0" + diffMin : diffMin) + " : " + (diffSec < 10 ? "0" + diffSec : diffSec) + " . " + (diffMsec < 10 ? "0" + diffMsec : diffMsec));
+        previousMin = minutes;
+        previousSec = seconds;
+        previousMsec = ms;
+
+        return (diffTime);
+    }
+
+    //for finding minimum and maximum time
+    function MinMaxOfLapTime(cTime, Id) {
+
+        if (Id == 1) {
+            previousMinimum = cTime;
+            previousMinimumId = Id;
+            previousMaximum = cTime;
+            previousMaximumId = Id;
+        }
+
+        else {
+            //Minimum
+            if (cTime < previousMinimum) {
+                previousMinimum = cTime;
+                var minimumValues = document.querySelectorAll(".minimum");
+                for (var i = 0; i < minimumValues.length; i++) {
+                    minimumValues[i].classList.remove("minimum");
+                }
+                previousMinimumId = Id;
+                document.getElementById(previousMinimumId).classList.add("minimum");
+            }
+
+            else if (cTime == previousMinimum) {
+                document.getElementById(Id).classList.add("minimum");
+            }
+
+            else {
+                document.getElementById(previousMinimumId).classList.add("minimum");
+            }
+
+            //Maximum
+
+            if (cTime > previousMaximum) {
+                previousMaximum = cTime;
+                var maxValues = document.querySelectorAll(".max");
+                for (var i = 0; i < maxValues.length; i++) {
+                    maxValues[i].classList.remove("max");
+                }
+                previousMaximumId = Id;
+                document.getElementById(previousMaximumId).classList.add("max");
+            }
+
+            else if (cTime == previousMaximum) {
+                document.getElementById(Id).classList.add("max");
+            }
+
+            else {
+                document.getElementById(previousMaximumId).classList.add("max");
+            }
+
+        }
+
     }
 }
